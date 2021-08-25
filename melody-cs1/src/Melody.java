d92017084e320f2e1f133635a3257c060116abb1
Anurag Purkayastha <anuragpurkayastha@gmail.com>
2021-08-25 15:03:31 +1000

    Added private method to return an array with elements being all of the notes to be played in order, taking into account repetition

--- melody-cs1/src/Melody.java
+++ melody-cs1/src/Melody.java
@@ -4,7 +4,7 @@ import melody.audio.*;
 
 public class Melody {
 
-    private Note[] notes;
+    private Note[] notes;       // Notes contained in the file listed in order. Does not account for repetitions.
     private String artist;
     private String title;
     private int numNotes;
@@ -84,90 +84,149 @@ public class Melody {
     }
 
     public String getArtist() {
-        // TODO: write this method
         return artist;
     }
 
-	public String getTitle() {
-        // TODO: write this method
+    public String getTitle() {
         return title;
     }
 
     public double getTotalDuration() {
-    /*
-    * Get the total duration of the song
-    *
-    * Loop through each note in the _notes_ array and sum the durations of each note.
-    */
-
-    double totalDuration = 0.0;
-    Note currNote = null;
-
-    // Is the current note within the section that will be repeated?
-    boolean inRepeatedSection = false;
-
-    // Loop through the notes of the melody
-    for ( int i = 0; i < notes.length; i++ ){
-        currNote = notes[i];
-        //System.out.println("Current note: " + currNote.toString());
         /*
-          Within a repeated section, each note is played twice.
-          If the current note is within a repeated section, then add twice the duration to the totalDuration.
+        * Get the total duration of the song
+        *
+        * Loop through each note in the _notes_ array and sum the durations of each note.
         */
 
-        // Check if the current note is the start of a repeated section. If it is, then toggle the inRepeatedSection flag (false -> true). If the current note is another repeated, toggle the flag to false (not in repeated section).
-        if ( currNote.isRepeat() ) {
+        double totalDuration = 0.0;
+        Note currNote = null;
 
-            if ( !inRepeatedSection ){
-                inRepeatedSection = true;
+        // Is the current note within the section that will be repeated?
+        boolean inRepeatedSection = false;
+
+        // Loop through the notes of the melody
+        for ( int i = 0; i < notes.length; i++ ){
+            currNote = notes[i];
+            //System.out.println("Current note: " + currNote.toString());
+            /*
+              Within a repeated section, each note is played twice.
+              If the current note is within a repeated section, then add twice the duration to the totalDuration.
+            */
+
+            // Check if the current note is the start of a repeated section. If it is, then toggle the inRepeatedSection flag (false -> true). If the current note is another repeated, toggle the flag to false (not in repeated section).
+            if ( currNote.isRepeat() ) {
+
+                if ( !inRepeatedSection ){
+                    inRepeatedSection = true;
+                }
+                else if ( inRepeatedSection ) {
+                    //System.out.println("Adding " + 2 * currNote.getDuration() + "s");
+                    totalDuration += 2 * currNote.getDuration();
+                    inRepeatedSection = false;
+                }
             }
-            else if ( inRepeatedSection ) {
+
+            if ( inRepeatedSection ){
                 //System.out.println("Adding " + 2 * currNote.getDuration() + "s");
                 totalDuration += 2 * currNote.getDuration();
-                inRepeatedSection = false;
+            }
+            else if ( !currNote.isRepeat() ){
+                //System.out.println("Adding " + currNote.getDuration() + "s");
+                totalDuration += currNote.getDuration();
             }
         }
-
-        if ( inRepeatedSection ){
-            //System.out.println("Adding " + 2 * currNote.getDuration() + "s");
-            totalDuration += 2 * currNote.getDuration();
-        }
-        else if ( !currNote.isRepeat() ){
-            //System.out.println("Adding " + currNote.getDuration() + "s");
-            totalDuration += currNote.getDuration();
-        }
-    }
-    return totalDuration;
+        
+        return totalDuration;
     }
 
     public boolean octaveDown() {
-            // TODO: write this method
-            return false;
+        // TODO: write this method
+        return false;
     }
 
     public boolean octaveUp() {
-            // TODO: write this method
-            return false;
+        // TODO: write this method
+        return false;
     }
 
     public void play() {
-  /*
-   * Play the melody.
-   * Loop through the array in _notes_ array and play each note.
-   */
-
-  for ( int i = 0; i < notes.length; i++ ){
-      notes[i].play();
-  }
+    /*
+    * Play the melody.
+    * Loop through the array in _notes_ array and play each note.
+    */
+        for ( int i = 0; i < notes.length; i++ ){
+            notes[i].play();
+        }
     }
 
     public void reverse() {
-            // TODO: write this method
+        // TODO: write this method
     }
 
     public String toString() {
-            // TODO: write this method
-            return "";
+        // TODO: write this method
+        
+        Note[] notesPlayed = this.melodyNotesToPlay();
+        String notesPlayedString = "";
+
+        for ( int i = 0; i < notesPlayed.length; i++ ){
+            notesPlayedString += notesPlayed[i].toString();
+            notesPlayedString += "\n";
+        }
+        return notesPlayedString;
+    }
+
+    private Note[] melodyNotesToPlay(){
+        /*
+         * Parse the melody file to create a note array with all of the notes to play in order.
+         *
+         * This takes into account the repeated sections.
+         */
+
+        // Take a copy of the notes list so we don't modify the original
+        Note[] copyOfNotes = notes;
+
+        ArrayList<Note> noteList = new ArrayList<Note>();
+        
+        // Loop through each note contained in the _notes_ array
+        int note_i = 0;  // Index of the current note.
+        Note currNote;  // Current note.
+        int rep_sec_start = 0;  // If the note is part of a repeated section => index of the starting note
+
+        // If the note is part of a repeated section, is this note the end of the repeated section? False if no, True if yes.
+        boolean isEndOfRepeat = true;
+
+        while ( note_i < copyOfNotes.length ){
+            currNote = copyOfNotes[note_i];
+            //System.out.println("Current note index: " + note_i);
+
+            // Add the current note to the array list
+            noteList.add(currNote);
+            
+            // If the current note is a repeat, toggle the flag if the note is the end of the repeat
+            if ( currNote.isRepeat() ){
+                isEndOfRepeat = !isEndOfRepeat; 
+
+                if ( !isEndOfRepeat ){
+                    rep_sec_start = note_i;
+                }
+            }
+
+            // If the current note is FALSE (ie. the start of the repeated section), then record the index of the current note as the _START_ of the section. Otherwise, record it as the _END_.
+            if ( isEndOfRepeat && currNote.isRepeat() ){
+                note_i = rep_sec_start;
+            }
+            else{
+                note_i++;
+            }
+
+            currNote.setRepeat(false);
+        }
+
+        Note[] noteListArray = new Note[noteList.size()];
+        noteListArray = noteList.toArray(noteListArray);
+        
+        return noteListArray;
     }
 }
 
